d85e9db0d5af9938986915cb84c3498da0c3650f
Peter Bourget <pbourget@w6op.com>
2022-08-07 07:45:51 -0700

    Minimum iOS release now 14

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "CallParser",
   platforms: [
     .macOS(.v12),
-    .iOS(.v13)
+    .iOS(.v14)
   ],
   products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
