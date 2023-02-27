be1132f854edaab5da60695503db169c5ed9246e
Peter Bourget <pbourget@w6op.com>
2023-02-27 11:45:18 -0800

    Throttle geocode requests.

--- Sources/CallParser/GeoManager.swift
+++ Sources/CallParser/GeoManager.swift
@@ -9,12 +9,17 @@ import Foundation
 import CoreLocation
 import os
 
+extension Task where Success == Never, Failure == Never {
+    static func sleep(seconds: Double) async throws {
+        let duration = UInt64(seconds * 1_000_000_000)
+        try await Task.sleep(nanoseconds: duration)
+    }
+}
+
 class GeoManager {
   let logger = Logger(subsystem: "com.w6op.CallParser", category: "GeoManager")
 
   var addressCache: [String: (latitude: Double, longitude: Double)] = [:]
-  var throttleTimerInterval = 60
-  var throttleTimerExpired = true
 
   init() {}
 
@@ -32,35 +37,26 @@ class GeoManager {
       return addressCache[address] ?? coordinates
     }
 
-    guard throttleTimerExpired else { return coordinates }
+    // prevent throttling when large numbers of requests are mad
+    do {
+      try await Task.sleep(seconds: 0.25)
+    } catch {
+      return coordinates
+    }
 
-      do {
-        location = try await geocoder.geocodeAddressString(address)
-          .compactMap( { $0.location } )
-          .first(where: { $0.horizontalAccuracy >= 0 } )!
+    do {
+      location = try await geocoder.geocodeAddressString(address)
+        .compactMap( { $0.location } )
+        .first(where: { $0.horizontalAccuracy >= 0 } )!
 
-        let coordinate = location.coordinate
-        coordinates.latitude = coordinate.latitude
-        coordinates.longitude = coordinate.longitude
-
-        addressCache[address] = coordinates
-        print(coordinates)
-      } catch{
-        print("the error is: \(error.localizedDescription)")
-      }
-
-//    guard let location = try await geocoder.geocodeAddressString(address)
-//      .compactMap( { $0.location } )
-//      .first(where: { $0.horizontalAccuracy >= 0 } )
-//    else {
-//      throw CLError(.geocodeFoundNoResult)
-//    }
-
-//    let coordinate = location.coordinate
-//    coordinates.latitude = coordinate.latitude
-//    coordinates.longitude = coordinate.longitude
-//
-//    addressCache[address] = coordinates
+      let coordinate = location.coordinate
+      coordinates.latitude = coordinate.latitude
+      coordinates.longitude = coordinate.longitude
+      addressCache[address] = coordinates
+    } catch {
+      //print("the error is: \(error.localizedDescription)")
+      return coordinates
+    }
 
     return coordinates
   }
